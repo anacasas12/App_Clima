87daa133f25bbf39c54734872a39978fa7a38fbe
anacasas12 <ana.casas@tecsup.edu.pe>
2022-12-02 23:26:47 -0500

    Initial Commit

---
+++ App Clima/App_ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ClimaApp.swift
+//  App Clima
+//
+//  Created by Jorge Herrera Jara on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct App_ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
